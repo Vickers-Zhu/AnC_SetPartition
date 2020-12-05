3fb2411f21e54b30f0d3e064f1eaf17c18d770a8
Vickers Zhu <vickerszhu@gmail.com>
2020-12-05 03:44:06 +0100

    Correct 1st version.

--- code/main.py
+++ code/main.py
@@ -5,16 +5,8 @@ from methods import generate_plot
 
 
 if __name__ == '__main__':
-    input_path = "./"
-    output_path = "./"
-
-#     x1 = [1, 2, 3, 4, 5, 6]
-#     y1 = [0.001149, 0.001285, 0.008426, 0.0011493, 0.000379, 0.003285]
-#     x = [2000, 4000, 6000, 8000, 10000]
-#     y = [0.020125, 0.088366, 0.144212, 0.260007, 0.380806]
-
-#     generate_csv(10000, input_path)
+    input_path = "../test/input"
+    output_path = "../test/output"
 
     run(input_path, output_path)
 
-#     generate_plot(x, y)
--- code/run.py
+++ code/run.py
@@ -1,8 +1,8 @@
 import os
-import csv
 import time
-from heuristics.gd_partition import GdPartition
-from exact.ex_partition import ExPartition
+from code.heuristics.gd_partition import GdPartition
+from code.exact.ex_partition import ExPartition
+from code.methods import generate_input
 
 
 def run(input_path, output_path):
@@ -17,49 +17,69 @@ def run(input_path, output_path):
 
 
 def read_and_load(input_path, input_data):
-    input_1 = os.path.join(input_path, "test1.csv")
-    input_2 = os.path.join(input_path, "test2.csv")
-    inputpath = ""
-    if os.path.exists(input_1):
-        inputpath = input_1
-        input_data['option'] = 1
-    elif os.path.exists(input_2):
-        inputpath = input_2
-        input_data['option'] = 2
-    input_array = []
-    with open(inputpath, 'r', encoding='utf-8-sig') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            for i in row:
-                input_array.append(int(i))
-    input_data['array'] = input_array
+    print('Read and Load')
+    f_list = os.listdir(input_path)
+    lines = []
+    new_lines = []
+    for i in f_list:
+        if os.path.splitext(i)[1] == '.txt':
+            f = open(os.path.join(input_path, i), 'r')
+            lines = lines + f.readlines()
+            f.close()
+    for line in lines:
+        new_str = str(line).strip().replace('\t', '').replace('\r', '').replace('\n', '')
+        if new_str == "":
+            continue
+        new_lines.append(new_str)
+    i = 0
+    atom = []
+    inputs = []
+    for line in new_lines:
+        i = i + 1
+        if i == 1:
+            continue
+        elif i == 2:
+            atom.append(line)
+        elif i == 3:
+            atom.append(int(line))
+        elif i == 4:
+            arr_str = line.split(',')
+            arr = []
+            for st in arr_str:
+                arr.append(int(st))
+            atom.append(arr)
+            inputs.append(atom)
+            atom = []
+            i = 0
+    input_data['input'] = inputs
 
 
 def calc(input_data, output_data):
     print("calculation")
-    pt = ""
-    start = ""
-    if input_data['option'] == 1:
-        print("Step: 1")
-        pt = ExPartition(input_data['array'])
+    outputs = []
+    for atom in input_data['input']:
+        input_array = ""
+        if len(atom[2]) == 1:
+            input_array = generate_input(atom[2][0])
+        else:
+            input_array = atom[2]
+        pt = ""
+        if atom[0] == 'o':
+            pt = ExPartition(input_array)
+        else:
+            pt = GdPartition(input_array)
+
         start = time.process_time()
         pt.partition()
-        output_data['time'] = format(time.process_time() - start, 'f')
-    elif input_data['option'] == 2:
-        print("Step: 2")
-        pt = GdPartition(input_data['array'])
-        start = time.process_time()
-        pt.partition()
-        output_data['time'] = format(time.process_time() - start, 'f')
-    output = []
-    for i in range(0, 4):
-        output.append(pt.p[i])
-    output_data['result'] = output
+        output = {'result': pt.p,
+                  'time': format(time.process_time() - start, 'f')}
+        outputs.append(output)
+        output_data['output'] = outputs
 
 
 def save(output_data, output_path):
     print("save")
-    with open(os.path.join(output_path, "result.csv"), 'w') as file:
-        writer = csv.writer(file)
-        writer.writerows(output_data['result'])
-        writer.writerow("Time: "+str(output_data['time']))
+    f = open(os.path.join(output_path, 'result.txt'), 'w')
+    for line in output_data['output']:
+        f.write(str(line) + '\n')
+    f.close()
